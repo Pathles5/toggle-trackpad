6f8366d0672b6feb314d56ffd77b903c920a8904
Apache Vegano <MY_NAME@example.com>
2025-10-08 13:38:21 +0200

    update libraries

--- src/index.tsx
+++ src/index.tsx
@@ -1,38 +1,32 @@
-import { definePlugin,  PanelSection, PanelSectionRow, ToggleField } from "@decky/ui";
+import { definePlugin } from "@decky/api";
+import { PanelSection, PanelSectionRow, ToggleField } from "@decky/ui";
 import { FaGamepad } from "react-icons/fa";
 import { useState } from "react";
 
 const ToggleDemo = () => {
-    const [enabled, setEnabled] = useState(false);
+  const [enabled, setEnabled] = useState(false);
 
-    return (
-        <>      
-            <PanelSection title="Demo">
-                <PanelSectionRow>
-                    <ToggleField
-                        label="Activar Trackpad"
-                        checked={enabled}
-                        onChange={(val: boolean) => {
-                            setEnabled(val);
-                            console.log("Toggle cambiado:", val);
-                        }}
-                    />
-                </PanelSectionRow>
-            </PanelSection>
-        </>
-    );
+  return (
+    <PanelSection title="Demo">
+      <PanelSectionRow>
+        <ToggleField
+          label="Activar Trackpad"
+          checked={enabled}
+          onChange={(val: boolean) => {
+            setEnabled(val);
+            console.log("Toggle cambiado:", val);
+          }}
+        />
+      </PanelSectionRow>
+    </PanelSection>
+  );
 };
 
 export default definePlugin(() => {
-    return {
-        name: "Toggle Trackpad",
-        title: <div>Toggle Trackpad TTL</div>,
-        titleView: <div>Toggle Trackpad TBL</div>,
-        content: <ToggleDemo />,   // 👈 aquí usamos el componente
-        icon: <FaGamepad />,
-        alwaysRender: true,
-        onDismount() {
-            console.log("Toggle Trackpad Plugin unmounted");
-        },
-    };
+  return {
+    name: "Toggle Trackpad",
+    title: <div className="title">Toggle Trackpad</div>,
+    content: <ToggleDemo />,
+    icon: <FaGamepad />,
+  };
 });
