6640804bf7b9f2e7c1a77487ce3dcaf4f6f69a04
Apache Vegano <MY_NAME@example.com>
2025-10-08 15:02:38 +0200

    test simplify index without components

--- dist/index.js
+++ dist/index.js
@@ -60,16 +60,11 @@ function FaGamepad (props) {
   return GenIcon({"attr":{"viewBox":"0 0 640 512"},"child":[{"tag":"path","attr":{"d":"M480.07 96H160a160 160 0 1 0 114.24 272h91.52A160 160 0 1 0 480.07 96zM248 268a12 12 0 0 1-12 12h-52v52a12 12 0 0 1-12 12h-24a12 12 0 0 1-12-12v-52H84a12 12 0 0 1-12-12v-24a12 12 0 0 1 12-12h52v-52a12 12 0 0 1 12-12h24a12 12 0 0 1 12 12v52h52a12 12 0 0 1 12 12zm216 76a40 40 0 1 1 40-40 40 40 0 0 1-40 40zm64-96a40 40 0 1 1 40-40 40 40 0 0 1-40 40z"},"child":[]}]})(props);
 }
 
-const ToggleDemo = () => {
-    const [enabled, setEnabled] = SP_REACT.useState(false);
-    return (window.SP_REACT.createElement(DFL.PanelSection, { title: "Demo" },
-        window.SP_REACT.createElement(DFL.PanelSectionRow, null,
-            window.SP_REACT.createElement(DFL.ToggleField, { label: "Activar Trackpad", checked: enabled, onChange: (val) => setEnabled(val) }))));
-};
 var index = DFL.definePlugin(() => {
     return {
         title: window.SP_REACT.createElement("div", { className: "title" }, "Toggle Trackpad"),
-        content: window.SP_REACT.createElement(ToggleDemo, null),
+        content: window.SP_REACT.createElement(DFL.PanelSection, { title: "Demo" },
+            window.SP_REACT.createElement(DFL.PanelSectionRow, null, "PANeL")),
         icon: window.SP_REACT.createElement(FaGamepad, null),
     };
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,16 @@
 import { definePlugin } from "@decky/ui";
-import { PanelSection, PanelSectionRow, ToggleField } from "@decky/ui";
+import { PanelSection, PanelSectionRow } from "@decky/ui";
 import { FaGamepad } from "react-icons/fa";
-import { useState } from "react";
-
-const ToggleDemo = () => {
-  const [enabled, setEnabled] = useState(false);
-
-  return (
-    <PanelSection title="Demo">
-      <PanelSectionRow>
-        <ToggleField
-          label="Activar Trackpad"
-          checked={enabled}
-          onChange={(val: boolean) => setEnabled(val)}
-        />
-      </PanelSectionRow>
-    </PanelSection>
-  );
-};
 
 export default definePlugin(() => {
   return {
     title: <div className="title">Toggle Trackpad</div>,
-    content: <ToggleDemo />,
+    content: 
+    <PanelSection title="Demo">
+      <PanelSectionRow>
+        PANeL
+      </PanelSectionRow>
+    </PanelSection>,
     icon: <FaGamepad />,
   };
 });
