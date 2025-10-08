66237a0f693b650bda8cfcf0b7113c771b42211d
Apache Vegano <MY_NAME@example.com>
2025-10-08 23:49:45 +0200

    test moving main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import logging
+import subprocess
+from decky_plugin import Plugin
+
+class Plugin:
+    async def activate(self):
+        logging.info("Modificando preset...")
+        subprocess.run(["python3", "/home/deck/.local/share/decky-loader/plugins/my-controller-plugin/backend/on.py"])
+
+    async def restore(self):
+        logging.info("Restaurando preset...")
+        subprocess.run(["python3", "/home/deck/.local/share/decky-loader/plugins/my-controller-plugin/backend/off.py"])
