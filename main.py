fe6ee4139a0e5108637c486c9390e1b49c15a345
Apache Veg(SD) <you@example.com>
2025-10-09 10:38:42 +0200

    route of backend linux

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ class Plugin:
     async def activate(self):
         logging.info("Modificando preset...")
         print("Modificando preset...")
-        subprocess.run(["python3", "/home/deck/.local/share/decky-loader/plugins/Toggle-Trackpad/backend/on.py"])
+        subprocess.run(["python3", "/home/deck/homebrew/plugins/Toggle-Trackpad/backend/on.py"])
 
     async def restore(self):
         logging.info("Restaurando preset...")
-        subprocess.run(["python3", "/home/deck/.local/share/decky-loader/plugins/Toggle-Trackpad/backend/off.py"])
+        subprocess.run(["python3", "/home/deck/homebrew/plugins/Toggle-Trackpad/backend/off.py"])
