f792f4ea0ccb1b384064fb0584d67eecaa294fab
Apache Vegano <MY_NAME@example.com>
2025-10-09 11:44:34 +0200

    ...

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import logging
 import subprocess
-from decky_plugin import Plugin
+# from decky_plugin import Plugin
+import decky
 
 class Plugin:
     async def _main(self):
-        decky_plugin.logger.info(" - - - - Framegen plugin loaded")
+        decky.logger.info(" - - - - Framegen plugin loaded")
 
     async def _unload(self):
-        decky_plugin.logger.info(" - - - - Framegen plugin unloaded.")
+        decky.logger.info(" - - - - Framegen plugin unloaded.")
 
     async def activate(self):
+        decky.logger.info("Modificando preset...")
         logging.info("Modificando preset...")
         print("Modificando preset...")
         subprocess.run(["python3", "/home/deck/homebrew/plugins/Toggle-Trackpad/backend/on.py"])
