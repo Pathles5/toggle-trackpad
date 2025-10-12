3dc77555dbb315d500559d2d54fb55e09be3a987
Apache Vegano <MY_NAME@example.com>
2025-10-12 13:27:53 +0200

    testing

--- src/index.tsx
+++ src/index.tsx
@@ -7,26 +7,26 @@ import "@decky/ui";
 
 console.log("[Decky UI] Available exports:", DeckyUI);
 // console.log("[Decky UI] Available exports:", AppOverview);
-console.log("SteamClient.Apps.StreamGame:"+SteamClient.Apps.StreamGame)
-console.log("SteamClient.Apps.ShowControllerConfigurator:"+SteamClient.Apps.ShowControllerConfigurator)
-console.log("SteamClient.Auth.GetMachineID:"+SteamClient.Auth.GetMachineID)
-console.log("SteamClient.Input.DuplicateControllerConfigurationSourceMode:"+SteamClient.Input.DuplicateControllerConfigurationSourceMode)
-console.log("SteamClient.Input.ExportCurrentControllerConfiguration:"+SteamClient.Input.ExportCurrentControllerConfiguration)
-console.log("SteamClient.Input.PreviewConfigForAppAndController:"+SteamClient.Input.PreviewConfigForAppAndController)
-console.log("SteamClient.Input.RegisterForShowControllerLayoutPreviewMessages:"+SteamClient.Input.RegisterForShowControllerLayoutPreviewMessages)
-console.log("SteamClient.Input.RegisterForUnboundControllerListChanges:"+SteamClient.Input.RegisterForUnboundControllerListChanges)
-console.log("SteamClient.Input.ResetControllerBindings:"+SteamClient.Input.ResetControllerBindings)
-console.log("SteamClient.Input.SaveEditingControllerConfiguration:"+SteamClient.Input.SaveEditingControllerConfiguration)
-console.log("SteamClient.Input.SetControllerPersonalizationSetting:"+SteamClient.Input.SetControllerPersonalizationSetting)
-console.log("SteamClient.Input.SetEditingControllerConfigurationActionSet:"+SteamClient.Input.SetEditingControllerConfigurationActionSet)
-console.log("SteamClient.Input.SetEditingControllerConfigurationInputBinding:"+SteamClient.Input.SetEditingControllerConfigurationInputBinding)
-console.log("SteamClient.Input.ShowControllerSettings:"+SteamClient.Input.ShowControllerSettings)
+// console.log("SteamClient.Apps.StreamGame:"+SteamClient.Apps.StreamGame) //Stremear game:     (appId: number, clientId: string, param2: number): void;?
+// console.log("SteamClient.Apps.ShowControllerConfigurator:"+SteamClient.Apps.ShowControllerConfigurator) // (appId: number): void;
+// console.log("SteamClient.Input.DuplicateControllerConfigurationSourceMode:"+SteamClient.Input.DuplicateControllerConfigurationSourceMode) // ??
+// console.log("SteamClient.Input.ExportCurrentControllerConfiguration:"+SteamClient.Input.ExportCurrentControllerConfiguration) // depend appId
+// console.log("SteamClient.Input.PreviewConfigForAppAndController:"+SteamClient.Input.PreviewConfigForAppAndController) // depend appId
+// console.log("SteamClient.Input.RegisterForShowControllerLayoutPreviewMessages:"+SteamClient.Input.RegisterForShowControllerLayoutPreviewMessages)
+// console.log("SteamClient.Input.RegisterForUnboundControllerListChanges:"+SteamClient.Input.RegisterForUnboundControllerListChanges)
+// console.log("SteamClient.Input.ResetControllerBindings:"+SteamClient.Input.ResetControllerBindings)
+// console.log("SteamClient.Input.SaveEditingControllerConfiguration:"+SteamClient.Input.SaveEditingControllerConfiguration)
+// console.log("SteamClient.Input.SetControllerPersonalizationSetting:"+SteamClient.Input.SetControllerPersonalizationSetting)
+// console.log("SteamClient.Input.SetEditingControllerConfigurationActionSet:"+SteamClient.Input.SetEditingControllerConfigurationActionSet)
+// console.log("SteamClient.Input.SetEditingControllerConfigurationInputBinding:"+SteamClient.Input.SetEditingControllerConfigurationInputBinding)
 
-console.log("SteamClient.Settings.GetAccountSettings:"+SteamClient.Settings.GetAccountSettings)
-console.log("SteamClient.Settings.GetCurrentLanguage:"+SteamClient.Settings.GetCurrentLanguage)
-console.log("SteamClient.System.GetSystemInfo:"+SteamClient.System.GetSystemInfo)
-console.log("SteamClient.User:"+SteamClient.User)
-console.log("SteamClient.WebChat.GetCurrentUserAccountID:"+SteamClient.WebChat.GetCurrentUserAccountID)
+console.log("SteamClient.Auth.GetMachineID:"+ await SteamClient.Auth.GetMachineID())
+console.log("SteamClient.Input.ShowControllerSettings:"+SteamClient.Input.ShowControllerSettings())
+console.log("SteamClient.Settings.GetAccountSettings:"+await SteamClient.Settings.GetAccountSettings())
+console.log("SteamClient.Settings.GetCurrentLanguage:"+await SteamClient.Settings.GetCurrentLanguage())
+console.log("SteamClient.System.GetSystemInfo:"+await SteamClient.System.GetSystemInfo())
+console.log("SteamClient.User:"+await SteamClient.User.GetLoginUsers())
+console.log("SteamClient.WebChat.GetCurrentUserAccountID:"+await SteamClient.WebChat.GetCurrentUserAccountID())
 
 export default definePlugin(() => {
 
